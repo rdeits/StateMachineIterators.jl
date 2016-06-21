c6ce0fd09bf84293b5872292074f1082cdb9da0b
Robin Deits <robin.deits@gmail.com>
2016-06-20 21:33:07 -0400

    initial import from the notebook

--- src/StateMachineIterators.jl
+++ src/StateMachineIterators.jl
@@ -1,5 +1,66 @@
 module StateMachineIterators
 
-# package code goes here
+import Base: start, done, next
+import DataStructures: OrderedDict
 
-end # module
+export StateMachine,
+    StateMachineIterator,
+    initial,
+    final,
+    add_transition!,
+    next_state
+
+type State
+    name::Symbol
+    transitions::OrderedDict{Function, State}
+end
+
+State(name::Symbol) = State(name, OrderedDict{Function, State}())
+
+type StateMachine
+    states::Dict{Symbol, State}
+    initial::State
+    final::State
+end
+
+initial(machine::StateMachine) = machine.initial.name
+final(machine::StateMachine) = machine.final.name
+
+function add_transition!(machine::StateMachine, from::Symbol, check::Function, to::Symbol)
+    from_state = get!(machine.states, from, State(from))
+    to_state = get!(machine.states, to, State(to))
+    from_state.transitions[check] = to_state
+end
+
+function StateMachine(initial::Symbol, final::Symbol, transitions::Vector{Tuple{Symbol, Function, Symbol}})
+    initial_state = State(initial)
+    final_state = State(final)
+    states = Dict{Symbol, State}(initial => initial_state, final => final_state)
+    machine = StateMachine(states, initial_state, final_state)
+    for (from, check, to) in transitions
+        add_transition!(machine, from, check, to)
+    end
+    machine
+end
+
+function next_state(state::State, input...)
+    for (check, destination) in state.transitions
+        check(input...) && return destination
+    end
+    state # if no transitions match, then return current state
+end
+
+next_state(machine::StateMachine, current_state::Symbol, input...) = next_state(machine.states[current_state], input...).name
+
+type StateMachineIterator
+    machine::StateMachine
+    input::Function
+end
+
+name_and_state(x::State) = (x.name, x)
+
+start(iter::StateMachineIterator) = iter.machine.initial
+done(iter::StateMachineIterator, state::State) = state == iter.machine.final
+next(iter::StateMachineIterator, state::State) = name_and_state(next_state(state, iter.input()...))
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,90 @@
 using StateMachineIterators
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Test non-iterator interface:
+
+let
+    machine = StateMachine(:going_up, :final,
+       [(:going_up, x -> x >= 10, :going_down),
+        (:going_down, x -> x <= 1, :final)])
+
+    behaviors = Dict(:going_up => x -> x + 1,
+                     :going_down => x -> x - 1,
+                     :final => x -> x)
+
+    values = []
+    x = 1
+    state = initial(machine)
+    while state != final(machine)
+        push!(values, x)
+        state = next_state(machine, state, x)
+        x = behaviors[state](x)
+    end
+
+    @test all(values .== [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+
+end
+
+let
+    machine = StateMachine(:going_up, :final,
+       [(:going_up, x -> x >= 5, :going_down),
+        (:going_down, x -> x <= 1, :final)])
+
+    behaviors = Dict(:going_up => x -> x + 1,
+                     :going_down => x -> x - 1,
+                     :final => x -> x)
+
+    values = []
+    x = 1
+    for state in StateMachineIterator(machine, () -> x)
+        push!(values, x)
+        x = behaviors[state](x)
+    end
+
+    @test all(values .== [1, 2, 3, 4, 5, 4, 3, 2, 1])
+end
+
+let
+    x_machine = StateMachine(:going_up, :final,
+       [(:going_up, x -> x >= 10, :going_down),
+        (:going_down, x -> x <= 1, :final)])
+
+    y_machine = StateMachine(:going_up, :final,
+       [(:going_up, y -> y >= 3, :going_down),
+        (:going_down, y -> y <= 1, :going_up)])
+
+    behaviors = Dict(:going_up => a -> a + 1,
+                     :going_down => a -> a - 1,
+                     :final => a -> a)
+
+    values = []
+    x = 1
+    y = 1
+    for (x_state, y_state) in zip(StateMachineIterator(x_machine, () -> x),
+                                  StateMachineIterator(y_machine, () -> y))
+        push!(values, (x, y))
+        x = behaviors[x_state](x)
+        y = behaviors[y_state](y)
+    end
+
+    @test all(values .== [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 2),
+        (5, 1),
+        (6, 2),
+        (7, 3),
+        (8, 2),
+        (9, 1),
+        (10, 2),
+        (9, 3),
+        (8, 2),
+        (7, 1),
+        (6, 2),
+        (5, 3),
+        (4, 2),
+        (3, 1),
+        (2, 2),
+        (1, 3)])
+end
